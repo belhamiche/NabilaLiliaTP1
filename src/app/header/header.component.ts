2a64e5a798bb4b11ae427a5c868663492128e14b
belhamiche <144937755+belhamiche@users.noreply.github.com>
2023-09-14 22:34:36 +0200

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 
 
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
-  header: string = 'header';
+  @Input() header! : string;
 
 
 }
