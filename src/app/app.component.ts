16975b122902a93c6e4eb0a176876f6228062d9a
belhamiche <144937755+belhamiche@users.noreply.github.com>
2023-09-14 22:35:33 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'NabilaLiliaTP1';
-
+  appfooter!: string;
+  appheader!: string;
   color : string ="white";
   public changeColor(newColor:string){
-    console.log(newColor);
     this.color = newColor;
+  }
+  ngOnInit() {
+    this.appfooter = 'footer';
+    this.appheader = 'header';
   }
 }
